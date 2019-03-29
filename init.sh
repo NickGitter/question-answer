2e26a42f43d62862c72af3af9cc2e5ddeb73310c
Kurabie <ezx363@yandex.ru>
2019-03-29 18:31:56 +0700

    Nginx should be installed. The command to install it has been added to the comments.

--- init.sh
+++ init.sh
@@ -3,9 +3,11 @@
     # ★★★ Question-answer app. ★★★
     # This is automatic script to initialize this app.
 
-echo -e "\033[33;1m* Question-answer app."
+echo -e "\033[33;1m* Question-answer app." # Yellow output.
 echo -e "* This script initializes the app.\033[0m"
 echo "Start initializing..."
+
+#sudo apt-get install nginx
 
 
 
