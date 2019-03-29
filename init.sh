f911dca30a6eccccb4652f3c5278e497fcfc166e
Kurabie <ezx363@yandex.ru>
2019-03-29 17:37:15 +0700

    Output info about the script (file == init.sh).

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-    # ★★★ Question-answer. ★★★
+    # ★★★ Question-answer app. ★★★
     # This is automatic script to initialize this app.
 
+echo "* Question-answer app."
+echo "* This script initializes the app."
+echo ""
+
+
+    
     # ★ ★ ★ ★ ★
 
 
