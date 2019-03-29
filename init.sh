bad9a6c2a19826fdea255fbb843c8bd4c1b6bc06
Kurabie <ezx363@yandex.ru>
2019-03-29 18:08:34 +0700

    Output messages about starting and completing initializing (file == init.sh).

--- init.sh
+++ init.sh
@@ -3,12 +3,15 @@
     # ★★★ Question-answer app. ★★★
     # This is automatic script to initialize this app.
 
-echo "* Question-answer app."
-echo "* This script initializes the app."
-echo ""
+echo -e "\033[36;1m* Question-answer app."
+echo -e "* This script initializes the app.\033[0m"
+echo "Start initializing..."
 
 
-    
+
+echo "Initializing complete."
+exit 0
+
     # ★ ★ ★ ★ ★
 
 
