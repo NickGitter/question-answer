f622467cddad54d3814a3041399faaac1b335f55
Kurabie <ezx363@yandex.ru>
2019-03-29 18:24:58 +0700

    Changed color of header output to yellow.

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
     # ★★★ Question-answer app. ★★★
     # This is automatic script to initialize this app.
 
-echo -e "\033[36;1m* Question-answer app."
+echo -e "\033[33;1m* Question-answer app."
 echo -e "* This script initializes the app.\033[0m"
 echo "Start initializing..."
 
@@ -12,6 +12,6 @@ echo "Start initializing..."
 echo "Initializing complete."
 exit 0
 
-    # ★ ★ ★ ★ ★
+    # ★ ★ ★ ★ ★ (^_^)
 
 
