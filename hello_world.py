72dc955789f6708c10081acca913c3dfdf5ac070
Kurabie <ezx363@yandex.ru>
2019-04-01 00:55:23 +0700

    ++ "hello world" application to starting with Gunicorn.

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+"
+    *** Hello world app to starting with Gunicorn. ***
+"
+
+def my_app(environ, start_response):
+    text = "Hello World!\n"
+    text = text.encode("utf-8")
+    status = "200 OK"
+    headers = [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(text)))
+    ]
+    start_response(status, headers)
+    return [ text ]
+
+
