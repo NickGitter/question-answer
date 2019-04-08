01450ba56ad12d3b1e45d81858889b4ff380adac
Kurabie <ezx363@yandex.ru>
2019-04-08 22:35:03 +0700

    ++ simple view my_test() for testing (file == ask/qa/views.py).

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+"""
+    *** MY VIEWS. ***
+"""
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def my_test(request, *args, **kwargs):
+    return HttpResponse('ok')
+
+
