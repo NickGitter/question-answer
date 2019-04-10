218fb1e5e286725724049d16b06963885bcb5fd6
Kurabie <ezx363@yandex.ru>
2019-04-10 20:27:24 +0700

    ++ model Question.

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,6 +5,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    def __unicode__(self):
+        return self.title
+    def get_absolute_url(self):
+        return '/question/%d/' % self.pk
+    class Meta:
+        ordering = ['-added_at']
 
 
