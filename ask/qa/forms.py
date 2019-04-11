865b8b86b935e5994291318cc4a5e5c0a367b701
Kurabie <ezx363@yandex.ru>
2019-04-11 16:20:35 +0700

    ++ form AnswerForm (file == forms.py).

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -31,4 +31,30 @@ class AskForm(forms.Form):
         question.save()
         return question
 
+class AnswerForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea)
+    question = forms.IntegerField(
+        widget=forms.HiddenInput(), initial=0) # Hidden field.
+    
+    def __init__(self, *args, **kwargs):
+        super(AnswerForm, self).__init__(*args, **kwargs)
+    
+    def clean(self):
+        return self.cleaned_data
+    
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        return text
+    
+    def clean_question(self):
+        question = self.cleaned_data['question']
+        return question
+    
+    def save(self, question, user):
+        self.cleaned_data['author'] = user
+        self.cleaned_data['question'] = question
+        answer = Answer(**self.cleaned_data)
+        answer.save()
+        return answer
+
 
