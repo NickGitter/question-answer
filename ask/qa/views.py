bf687cc7755d0c43ad32fc0b46c1748b3f4aaa87
Kurabie <ezx363@yandex.ru>
2019-04-11 12:11:22 +0700

    ++ view one_question() - the question page (file == views.py).

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,7 +2,7 @@
     *** MY VIEWS. ***
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
 
@@ -23,6 +23,20 @@ def popular_page(request, *args, **kwargs):
     questions = Question.objects.popular()
     return render(request, 'list_of_questions.html', {
         'questions': questions,
+    })
+
+def one_question(request, id):
+    question = get_object_or_404(Question, id=id)
+    try:
+        answers = Answer.objects.filter(question=question)
+        answers = answers.order_by('-added_at')
+        answers = answers[0:]
+    except Answer.DoesNotExist:
+        answers = []
+    return render(request, 'one_question.html', {
+        'title': question.title,
+        'text': question.text,
+        'answers': answers,
     })
 
 
