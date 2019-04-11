20ffbd5f9face6f638a12965bb59e24b0928f105
Kurabie <ezx363@yandex.ru>
2019-04-11 12:01:49 +0700

    ++ view main_page() (file == views.py).

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,8 +4,18 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_GET
+
+from qa.models import Question, Answer
 
 def my_test(request, *args, **kwargs):
     return HttpResponse('ok')
+
+@require_GET
+def main_page(request, *args, **kwargs):
+    questions = Question.objects.new()
+    return render(request, 'list_of_questions.html', {
+        'questions': questions,
+    })
 
 
