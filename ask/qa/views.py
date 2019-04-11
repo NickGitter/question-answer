79fb6cf4752de31900c4d02fef88ceffcd7be2da
Kurabie <ezx363@yandex.ru>
2019-04-11 12:16:25 +0700

    ++ function my_paginate(...) - it's function for abstract pagination (file == views.py).

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,10 +3,25 @@
 """
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator
 
 from qa.models import Question, Answer
+
+# Function for abstract pagination.
+def my_paginate(request, QuerySet):
+    limit = 10
+    try:
+        page = int( request.GET.get('page', 1) )
+    except ValueError:
+        raise Http404
+    paginator = Paginator(QuerySet, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page
 
 def my_test(request, *args, **kwargs):
     return HttpResponse('ok')
