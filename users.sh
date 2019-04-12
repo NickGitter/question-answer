1e85e9f3ad6f1bfb05fbdda0e8d2f52f19b26062
Kurabie <ezx363@yandex.ru>
2019-04-12 09:58:32 +0700

    ++ var "attribs" with the list of table attributes (file == users.sh).

--- users.sh
+++ users.sh
@@ -5,13 +5,23 @@
 dbName="qa_db"
 tableName="auth_user"
 
+attribsArr=(
+    "id"
+    "username"
+)
+
+attribs="${attribsArr[0]}"
+for iAttrib in ${attribsArr[@]:1:${#attribsArr[@]}}
+do
+    attribs="${attribs}, ${iAttrib}"
+done
+
 argsNum=$#
 
 if [[ $argsNum -eq 0 ]]
 then
-    sudo mysql -uroot -e "select * from ${dbName}.${tableName};"
+    sudo mysql -uroot -e "select ${attribs} from ${dbName}.${tableName};"
     exit 0
 fi
-    
-    
-    
+
+
