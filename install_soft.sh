ae7370c5ac8699040fccaba4ac9bcab2b904ca92
Kurabie <ezx363@yandex.ru>
2019-05-02 19:31:37 +0700

    Created script to install the soft for this app (file == install_soft.sh).

--- install_soft.sh
+++ install_soft.sh
@@ -3,6 +3,17 @@
 #   Script to install the software.
 #
 
+sudo apt-get install \
+    python3\
+    nginx\
+    gunicorn\
+    python-django\
+    python3-django\
+    mysql-server\
+    mysql-client\
+    python-mysqldb\
+    python3-mysqldb
 
+exit 0
 
 
